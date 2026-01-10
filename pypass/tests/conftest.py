7ea175455b63c2787919bee14cd8287a9240e5fd
Coryell Cottage <phoenix@coryellcottage.com>
2026-01-10 10:37:37 -0500

    Add --target flag so tests don't fail in github CI

--- pypass/tests/conftest.py
+++ pypass/tests/conftest.py
@@ -1,9 +1,3 @@
-import pytest
-
-
-#@pytest.fixture
-#def get_api_key(config):
-#    return config.getoption("--api-key"
-
 def pytest_addoption(parser):
     parser.addoption("--api-key", action="store", help="Github API key") 
+    parser.addoption("--target", required=False, help="Target platform for test suite (do not pass, only there so tests don't fail in github CI)")
