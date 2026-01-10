08267cb46b5b47b0c0d19ef06fd61cb9b43e7814
Spyridon Coryell <SpyridonCoryell@protonmail.com>
2026-01-10 10:48:03 -0500

    Removed unneeded flag for github CI, and move where API key gets passed

--- pypass/tests/conftest.py
+++ pypass/tests/conftest.py
@@ -1,3 +1,2 @@
 def pytest_addoption(parser):
-    parser.addoption("--api-key", action="store", help="Github API key") 
-    parser.addoption("--target", required=False, help="Target platform for test suite (do not pass, only there so tests don't fail in github CI)")
+    parser.addoption("--api-key", action="store", help="Github API key")
